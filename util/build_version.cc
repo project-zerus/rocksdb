5993550be3ca8a11b07abc694fac17ed3846fe89
liuhuahang <liuhuahang@zerus.co>
2014-08-11 19:52:34 +0800

    add blade build for rocksdb
    N/A
    
    Change-Id: I6e3fffc6f8ded49f82d893f3fee1cfebb10c2383
    Signed-off-by: liuhuahang <liuhuahang@zerus.co>

---
+++ util/build_version.cc
@@ -0,0 +1,5 @@
+#include "build_version.h"
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:91c01485d1c0a4675e24b39ad2adb950a3a468e6";
+const char* rocksdb_build_git_datetime = "rocksdb_build_git_datetime:Mon Aug 11 19:48:05 HKT 2014";
+const char* rocksdb_build_compile_date = __DATE__;
+const char* rocksdb_build_compile_time = __TIME__;
